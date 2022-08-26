917c38e0af560248199cdd76b5b6ad8c227a3725
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2022-08-26 21:10:42 +0200

    app.js completed

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
-const restartBtn = document.querySelector('.restart');
+let computerNb = Math.floor(Math.random() * 20);
+let userNb = 0;
+let score = 10;
 
-restartBtn.addEventListener('click', () => {
-  alert(getRandomInt(20));
+let totalScore = document.querySelector('.total_score');
+let infoMsg = document.querySelector('.displayer');
+
+const userBtn = document.querySelector('.user_btn');
+const userInput = document.querySelector('.user');
+
+userBtn.addEventListener('click', () => {
+  userNb = userInput.value;
+  if (userNb > 20) {
+    infoMsg.innerHTML = '20 max please !';
+    infoMsg.style.color = 'crimson';
+  } else {
+    whoWins(computerNb, userNb);
+  }
 });
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function whoWins(prog, user) {
+  if (user < prog) {
+    updateScore(1);
+  } else if (user > prog) {
+    updateScore(2);
+  } else {
+    infoMsg.innerHTML = 'You win !';
+    document.querySelector('.highest_score').innerHTML = score;
+  }
+  console.log('score =' + score);
 }
 
-let userNb = document.querySelector('#user');
-userNb.addEventListener('input', () => {
-  console.log(userNb.value);
+function updateScore(int) {
+  score -= 1;
+  totalScore.innerHTML = score;
+  infoMsg.style.color = '#EEC13A';
+  if (int == 1) {
+    infoMsg.innerHTML = 'Too low !';
+  } else if (int == 2) {
+    infoMsg.innerHTML = 'Too High !';
+  }
+}
+
+function reset() {
+  userNb = 0;
+  score = 10;
+  computerNb = Math.floor(Math.random() * 20);
+  infoMsg.innerHTML = 'Start playing...';
+  infoMsg.style.color = '#fff';
+  totalScore.innerHTML = 10;
+  userInput.value = '';
+}
+
+const restartBtn = document.querySelector('.restart');
+restartBtn.addEventListener('click', () => {
+  reset();
 });
